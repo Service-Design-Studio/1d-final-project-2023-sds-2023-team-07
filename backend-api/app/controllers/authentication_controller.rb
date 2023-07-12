5f25674680d358f26fd7b908f43c5845ff5b7d22
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-12 21:24:01 +0800

    POST images able to show that the name of the success authentication person

--- backend-api/app/controllers/authentication_controller.rb
+++ backend-api/app/controllers/authentication_controller.rb
@@ -1,12 +1,44 @@
 class AuthenticationController < ApplicationController
 
+    require 'aws-sdk-rekognition'
+    require 'base64'
+
     def face
         image_data = params[:image]
         if image_data.present?
-            puts image_data
-            render json: {message: "Success"}, status: :ok
+        name, confidence = find_person_using_image('face-id-test', image_data, 90)
+
+        if name
+            render json: {message: "Person found: #{name}, Confidence level: #{confidence}"}, status: :ok
         else
-            render json: {message: "Nothing received"}, status: :unprocessable_entity
+            render json: {message: 'No matching person found.'}, status: :unprocessable_entity
         end
+        else
+        render json: {message: "Nothing received"}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def find_person_using_image(collection_name, base64_image, thresholdval)
+        client = Aws::Rekognition::Client.new
+
+        # remove data:image/png;base64, if it exists and then decode the base64
+        image_bytes = Base64.decode64(base64_image.gsub(/^data:image\/\w+;base64,/, ''))
+
+        response = client.search_faces_by_image(
+        collection_id: collection_name,
+        face_match_threshold: thresholdval,
+        image: { bytes: image_bytes },
+        max_faces: 1
+        )
+
+        if response.face_matches.any?
+            name = response.face_matches[0].face.external_image_id
+            confidence = response.face_matches[0].similarity
+            return name, confidence
+        end
+
+        return nil, nil
     end
 end
