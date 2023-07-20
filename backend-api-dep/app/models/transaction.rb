86a782700e5fbc86ef5ef415795bf4f837278040
Ryan <ryanpey31@gmail.com>
2023-07-20 16:10:07 +0800

    Oppenheimer

--- backend-api-dep/app/models/transaction.rb
+++
@@ -1,7 +0,0 @@
-class Transaction < ApplicationRecord
-  belongs_to :user
-  belongs_to :atm_machine
-  enum transaction_type: { deposit: 0, withdrawal: 1, other: 2 }
-
-  validates :amount, presence: true, numericality: { greater_than: 0 }
-end
