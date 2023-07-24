01ce5cafd943878f76929487f124d29ef36a8774
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-24 22:41:13 +0800

    update routes for new controllers

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,20 +1,34 @@
 Rails.application.routes.draw do
-  post 'authenticate/face', to: 'authentication#face'
-  post 'authenticate/pin', to: 'authentication#pin'
-  post 'withdraw', to: 'transactions#withdraw'
-  post 'withdraw/confirm', to: 'transactions#confirm_withdrawal'
-  post 'deposit', to: 'transactions#deposit'
-  post 'deposit/confirm', to: 'transactions#confirm_deposit'
-  post 'voice_transaction', to: 'transactions#voice_transaction'
-  
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  # User authentication routes
+  post 'authenticate/face', to: 'authentication#face' # Handles face authentication
+  post 'authenticate/pin', to: 'authentication#pin'   # Handles pin authentication
+
+  # Transaction related routes
+  post 'withdraw', to: 'transactions#withdraw' # Handles withdrawal transactions
+  post 'deposit', to: 'transactions#deposit' # Handles deposit transactions
+
   resources :users do
-    resources :transactions, only: [:index]
+    resources :transactions, only: [:index, :show, :update, :destroy]
   end
+  # The above block will generate the following routes:
+  # GET '/users/:user_id/transactions' - List all transactions for a specific user
+  # GET '/users/:user_id/transactions/:id' - Show details of a specific transaction of a user
+  # PATCH '/users/:user_id/transactions/:id' - Update a specific transaction of a user
+  # DELETE '/users/:user_id/transactions/:id' - Delete a specific transaction of a user
 
-  resources :users, only: [:create] do
-    resources :transactions, only: [:index]
-  end
+  resources :atm_machines, except: [:new, :edit]
+  # This will generate all CRUD operations for atm_machines except for new and edit:
+  # GET '/atm_machines' - List all atm machines
+  # POST '/atm_machines' - Create a new atm machine
+  # GET '/atm_machines/:id' - Show details of a specific atm machine
+  # PATCH '/atm_machines/:id' - Update a specific atm machine
+  # DELETE '/atm_machines/:id' - Delete a specific atm machine
 
-
-  resources :atm_machines, only: [:create]
+  resources :transactions, only: [:show, :update, :destroy]
+  # This will generate show, update, and destroy operations for transactions:
+  # GET '/transactions/:id' - Show details of a specific transaction
+  # PATCH '/transactions/:id' - Update a specific transaction
+  # DELETE '/transactions/:id' - Delete a specific transaction
 end
