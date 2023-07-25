be346251714a69ba4802add0812a5af8e5f04e90
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-25 16:28:26 +0800

    implement chakra ui inside project

---
+++ atm-frontend/src/pages/_app.page.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+const theme = extendTheme({
+  colors: {
+    color1: "#094C3E",
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
