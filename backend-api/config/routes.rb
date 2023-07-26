bbc984fbbe3354da550f8cadb49c05bbf4a082c8
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-27 02:30:12 +0800

    modularize routes

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -6,26 +6,8 @@ Rails.application.routes.draw do
   post 'authenticate/pin', to: 'authentication#pin'   # Handles pin authentication
 
 
-  resources :users do
-    resources :transactions, only: [:index, :show, :update, :destroy]
-  end
-  # The above block will generate the following routes:
-  # GET '/users/:user_id/transactions' - List all transactions for a specific user
-  # GET '/users/:user_id/transactions/:id' - Show details of a specific transaction of a user
-  # PATCH '/users/:user_id/transactions/:id' - Update a specific transaction of a user
-  # DELETE '/users/:user_id/transactions/:id' - Delete a specific transaction of a user
+  resources :users
+  resources :atm_machines
+  resources :transactions
 
-  resources :atm_machines, except: [:new, :edit]
-  # This will generate all CRUD operations for atm_machines except for new and edit:
-  # GET '/atm_machines' - List all atm machines
-  # POST '/atm_machines' - Create a new atm machine
-  # GET '/atm_machines/:id' - Show details of a specific atm machine
-  # PATCH '/atm_machines/:id' - Update a specific atm machine
-  # DELETE '/atm_machines/:id' - Delete a specific atm machine
-
-  resources :transactions, only: [:create, :show, :update, :destroy]
-  # This will generate show, update, and destroy operations for transactions:
-  # GET '/transactions/:id' - Show details of a specific transaction
-  # PATCH '/transactions/:id' - Update a specific transaction
-  # DELETE '/transactions/:id' - Delete a specific transaction
 end
