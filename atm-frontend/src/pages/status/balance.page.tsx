a75bb7ec035dc79342fee4d20ba6c899665e23a5
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-26 19:26:42 +0800

    balance page add buttons and timeout

--- atm-frontend/src/pages/status/balance.page.tsx
+++ atm-frontend/src/pages/status/balance.page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Box, Button, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Balance = () => {
-  return <div>Balance</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 20000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  const handleGoHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      width="100vw"
+      p={3}
+    >
+      <Text fontSize="4xl" mb="4">
+        Your account balance is ...
+      </Text>
+      <Button backgroundColor="dbsRed" color="white" onClick={handleGoHome}>
+        Go Home
+      </Button>
+    </Box>
+  );
 };
 
 export default Balance;
