8629638b872f92ec924640f76a6a411cc3cbadd2
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-27 02:29:38 +0800

    atm machine comment route on controller

--- backend-api/app/controllers/atm_machines_controller.rb
+++ backend-api/app/controllers/atm_machines_controller.rb
@@ -1,5 +1,5 @@
 class AtmMachinesController < ApplicationController
-  # CREATE
+  # POST '/atm_machines' - Create a new ATM machine
   def create
     @atm_machine = AtmMachine.new(atm_machine_params)
     if @atm_machine.save
@@ -9,19 +9,20 @@ class AtmMachinesController < ApplicationController
     end
   end
 
-  # READ (Index - fetch all ATM machines)
+  # GET '/atm_machines' - List all ATM machines
   def index
     @atm_machines = AtmMachine.all
     render json: @atm_machines
   end
 
-  # READ (Show - fetch a specific ATM machine)
+  # GET '/atm_machines/:id' - Show details of a specific ATM machine
   def show
     @atm_machine = AtmMachine.find(params[:id])
     render json: @atm_machine
   end
 
-  # UPDATE
+  # PATCH '/atm_machines/:id' - Update a specific ATM machine
+  # PUT '/atm_machines/:id' - Update a specific ATM machine
   def update
     @atm_machine = AtmMachine.find(params[:id])
     if @atm_machine.update(atm_machine_params)
@@ -31,7 +32,7 @@ class AtmMachinesController < ApplicationController
     end
   end
 
-  # DELETE
+  # DELETE '/atm_machines/:id' - Delete a specific ATM machine
   def destroy
     @atm_machine = AtmMachine.find(params[:id])
     if @atm_machine.destroy
