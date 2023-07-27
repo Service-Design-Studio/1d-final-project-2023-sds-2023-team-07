050f4121586bc86636023bd95db98957e0883748
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-27 22:37:42 +0800

    transaction page done and main

---
+++ atm-frontend/src/pages/status/Transaction.page.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Text, Button, Center, useToast } from "@chakra-ui/react";
+import { TransactionData } from "@/types";
+
+const Transaction = () => {
+  const router = useRouter();
+  const { data } = router.query;
+
+  const [transaction, setTransaction] = useState<TransactionData | null>(null);
+  const toast = useToast();
+
+  const handleButtonClick = () => {
+    if (transaction) {
+      fetch("/api/postTransaction", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(transaction),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if ("errors" in data && data.errors === "Insufficient balance") {
+            return fetch(
+              `/api/patchUserIsActive?user_id=${transaction.user_id}`,
+              {
+                method: "PATCH",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ is_active: 2 }),
+              }
+            ); // then router push to NotEnoughMoneyFail
+          } else {
+            return fetch(
+              `/api/patchUserIsActive?user_id=${transaction.user_id}`,
+              {
+                method: "PATCH",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ is_active: 1 }),
+              }
+            ); // then router push to SuccessBalance
+          }
+        })
+        .then((response) => response.json())
+        .then((updatedUser) => {
+          if (updatedUser.is_active === 2) {
+            router.push({
+              pathname: "/status/NotEnoughMoneyFail",
+              query: { data: JSON.stringify(transaction) },
+            });
+          } else {
+            router.push({
+              pathname: "/status/SuccessBalance",
+              query: { data: JSON.stringify(transaction) },
+            });
+          }
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+
+  useEffect(() => {
+    if (data && typeof data === "string") {
+      let transactionData: TransactionData = JSON.parse(data);
+      transactionData.atm_machine_id = 1; // Add the atm_machine_id here
+
+      console.log(transactionData);
+      setTransaction(transactionData);
+    }
+
+    const timer = setTimeout(() => {
+      router.push("/status/TimeoutFail");
+    }, 20000);
+
+    return () => clearTimeout(timer);
+  }, [data, toast, router]);
+
+  return (
+    <Center height="100vh" flexDirection="column">
+      {transaction && transaction.transaction_type === "NCD" && (
+        <Text fontSize="xl">Deposit your cash now</Text>
+      )}
+      {transaction && transaction.transaction_type === "AWL" && (
+        <Text fontSize="xl">Withdraw ${transaction.amount} now</Text>
+      )}
+      <Button
+        backgroundColor="dbsRed"
+        textColor="white"
+        mt={4}
+        onClick={handleButtonClick}
+      >
+        Done
+      </Button>
+    </Center>
+  );
+};
+
+export default Transaction;
