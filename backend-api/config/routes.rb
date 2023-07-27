6405debf7e89215d64b8bd7cfa26799c83f1589e
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-07-27 19:51:48 +0800

    deployed using sqlite for sprint3 version

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   # User authentication routes
   post 'authenticate/face', to: 'authentication#face' # Handles face authentication
   post 'authenticate/pin', to: 'authentication#pin'   # Handles pin authentication
