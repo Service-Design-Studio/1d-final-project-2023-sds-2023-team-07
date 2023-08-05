2f035bb3feabfef3e646dea52546125e4b9fb451
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-08-06 03:57:19 +0800

    configs for enabling sessions

--- backend-api/config/application.rb
+++ backend-api/config/application.rb
@@ -21,6 +21,11 @@ module BackendApi
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
+    config.session_store :cookie_store, key: '_your_app_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_your_app_session'
+
+
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
