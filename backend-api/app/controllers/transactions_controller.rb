78d2f8e8ce062275ab8da7057493feb7b59bd615
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-08-08 16:03:42 +0800

    ensure fat model thin controller & cookies

--- backend-api/app/controllers/transactions_controller.rb
+++ backend-api/app/controllers/transactions_controller.rb
@@ -1,60 +1,29 @@
-require 'bigdecimal'
 class TransactionsController < ApplicationController
-  before_action :set_user, only: [:create]
+  before_action :set_current_user
+  before_action :set_transaction, only: [:show, :update, :destroy]
   before_action :set_atm_machine, only: [:create]
 
-  # GET '/transactions' - List all transactions
-  # GET '/transactions?user=1' - List all transactions for a specific user
+  # GET '/transactions'
   def index
-    if params[:user]
-      @user = User.find_by(id: params[:user])
-      if @user
-        @transactions = @user.transactions
-      else
-        render json: { error: "User not found" }, status: :unprocessable_entity and return
-      end
-    else
-      @transactions = Transaction.all
-    end
-    render json: @transactions
+    render json: @current_user.transactions
   end  
 
-  # POST '/transactions' - Create a new transaction
+  # POST '/transactions'
   def create
-    @transaction = Transaction.new(transaction_params)
-    if @transaction.save
-      render json: @transaction, status: :created
-    else
-      render json: {errors: @transaction.errors.full_messages}, status: :unprocessable_entity
-    end
+    @transaction = Transaction.create_for_user!(transaction_params, @current_user)
+    render json: @transaction, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
-  # def create
-  #   if params[:transaction_type] == 'AWL' && BigDecimal(transaction_params[:amount]) > @user.balance
-  #     render json: { errors: "Insufficient balance" }, status: :unprocessable_entity
-  #   else
-  #     Transaction.transaction do
-  #       @transaction = Transaction.new(transaction_params)
-  #       if @transaction.save
-  #         @user.update!(balance: @transaction.user_balance_left)
-  #         @atm_machine.update!(balance: @transaction.atm_balance_left)  # Update the AtmMachine balance
-  #         render json: @transaction, status: :created
-  #       else
-  #         render json: {errors: @transaction.errors.full_messages}, status: :unprocessable_entity
-  #       end
-  #     end
-  #   end
-  # end
 
-  # GET '/transactions/:id' - Show details of a specific transaction
+
+  # GET '/transactions/:id'
   def show
-    @transaction = Transaction.find(params[:id])
     render json: @transaction
   end
 
-  # PATCH '/transactions/:id' - Update a specific transaction
-  # PUT '/transactions/:id' - Update a specific transaction  
+  # PATCH '/transactions/:id' or PUT '/transactions/:id'
   def update
-    @transaction = Transaction.find(params[:id])
     if @transaction.update(transaction_params)
       render json: @transaction
     else
@@ -62,9 +31,8 @@ class TransactionsController < ApplicationController
     end
   end
 
-  # DELETE '/transactions/:id' - Delete a specific transaction
+  # DELETE '/transactions/:id'
   def destroy
-    @transaction = Transaction.find(params[:id])
     if @transaction.destroy
       render json: { message: 'Transaction deleted successfully' }, status: :ok
     else
@@ -72,12 +40,21 @@ class TransactionsController < ApplicationController
     end
   end
 
-  def set_user
-    @user = User.find_by(id: params[:user_id])
-    unless @user
-      render json: { error: "User not found" }, status: :not_found
+  private
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      render json: { error: "Not logged in" }, status: :unauthorized
     end
-  end  
+  end
+
+  def set_transaction
+    @transaction = Transaction.find_by(id: params[:id])
+    if @transaction.nil? || @transaction.user != @current_user
+      render json: { error: "Transaction not found or not authorized" }, status: :not_found
+    end
+  end
   
 
   def set_atm_machine
@@ -87,19 +64,7 @@ class TransactionsController < ApplicationController
     end
   end
 
-  # def transaction_params # raise error if params are not present
-  #   transaction_data = params.permit(:user_id, :atm_machine_id, :amount, :transaction_type)
-  #   if transaction_data[:transaction_type] == 'NCD'
-  #     transaction_data[:user_balance_left] = @user.balance + BigDecimal(transaction_data[:amount])
-  #     transaction_data[:atm_balance_left] = @atm_machine.balance + BigDecimal(transaction_data[:amount])
-  #   elsif transaction_data[:transaction_type] == 'AWL' && BigDecimal(transaction_data[:amount]) <= @user.balance
-  #     transaction_data[:user_balance_left] = @user.balance - BigDecimal(transaction_data[:amount])
-  #     transaction_data[:atm_balance_left] = @atm_machine.balance - BigDecimal(transaction_data[:amount])
-  #   end
-  #   transaction_data
-  # end
   def transaction_params
-    params.require(:transaction).permit(:user_id, :atm_machine_id, :amount, :transaction_type)
+    params.require(:transaction).permit(:atm_machine_id, :amount, :transaction_type)
   end
-
 end
--- backend-api/app/models/transaction.rb
+++ backend-api/app/models/transaction.rb
@@ -4,18 +4,47 @@ class Transaction < ApplicationRecord
 
   validates :transaction_type, inclusion: { in: ['AWL', 'NCD'] }
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  
+  # Custom validation
+  validate :sufficient_balance, if: -> { transaction_type == 'AWL' }
 
   after_create :update_balances
 
+  def self.create_for_user!(params, user)
+    transaction = new(params)
+    transaction.user = user
+    
+    # Check for ATM machine existence
+    unless AtmMachine.exists?(params[:atm_machine_id])
+      transaction.errors.add(:atm_machine_id, "ATM Machine not found")
+      raise ActiveRecord::RecordInvalid, transaction
+    end
+  
+    transaction.save!
+    transaction
+  end
+  
+
   private
 
+  def sufficient_balance
+    errors.add(:amount, "Insufficient balance") if BigDecimal(amount) > user.balance && BigDecimal(amount) > atm_machine.balance
+  end
+
   def update_balances
-    if transaction_type == 'NCD'
-      user.update!(balance: user.balance + BigDecimal(amount))
-      atm_machine.update!(balance: atm_machine.balance + BigDecimal(amount))
-    elsif transaction_type == 'AWL' && BigDecimal(amount) <= user.balance
-      user.update!(balance: user.balance - BigDecimal(amount))
-      atm_machine.update!(balance: atm_machine.balance - BigDecimal(amount))
+    ApplicationRecord.transaction do
+      if transaction_type == 'NCD'
+        user.increment!(:balance, BigDecimal(amount))
+        atm_machine.increment!(:balance, BigDecimal(amount))
+      else
+        user.decrement!(:balance, BigDecimal(amount))
+        atm_machine.decrement!(:balance, BigDecimal(amount))
+      end
+
+      # Updating the balance left fields for the transaction
+      self.user_balance_left = user.balance
+      self.atm_balance_left = atm_machine.balance
+      self.save!
     end
   end
 end
