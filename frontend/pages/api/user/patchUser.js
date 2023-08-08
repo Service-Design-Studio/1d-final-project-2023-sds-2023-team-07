9611a85548c751de9ea9bed1fa5564386b1666fb
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-08-09 00:13:29 +0800

    patch user on pages api done

--- frontend/pages/api/user/patchUser.js
+++ frontend/pages/api/user/patchUser.js
@@ -1,23 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req, res) => {
-  try {
-    const response = await fetch(
-      "https://backend-dbs-grp7-ml42q3c3ya-as.a.run.app/user",
-      {
-        credentials: "include",
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: req.body,
-      }
-    );
+  if (req.method === "PATCH") {
+    // Extract cookie from the incoming request
+    const userCookie = req.headers.cookie;
 
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } catch (error) {
-    console.error("Error in patchUser API:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    try {
+      // Use the cookie when making the external API request.
+      const response = await fetch(
+        "https://backend-dbs-grp7-ml42q3c3ya-as.a.run.app/user",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json", // Important for sending JSON data
+            Authorization: `Bearer ${userCookie}`,
+            Cookie: userCookie,
+          },
+          body: JSON.stringify(req.body), // Convert the request body to a JSON string
+        }
+      );
+
+      // Check if the fetch request was successful
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
 };
