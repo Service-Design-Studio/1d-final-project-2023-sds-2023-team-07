a6fbc8714c2c320cab2b352d9f2d2df1e3df99b9
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-08-10 22:06:47 +0800

    remove interval comment

--- frontend/app/stream/page.js
+++ frontend/app/stream/page.js
@@ -23,7 +23,7 @@ export default function Page() {
   const router = useRouter();
 
   const startCapture = () => {
-    // intervalId = setInterval(captureFrame, 500);
+    intervalId = setInterval(captureFrame, 500);
     console.log("capturing");
   };
 
