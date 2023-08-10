e87e892e8f8311057d70e64cac64bf075846cdba
Yong Lin <yonglintan@gmail.com>
2023-08-11 01:16:31 +0800

    Features

--- frontend/cypress/e2e/authentication/authentication.js
+++ frontend/cypress/e2e/authentication/authentication.js
@@ -1,127 +1,134 @@
-import { Given, Step, Then, When } from "@badeball/cypress-cucumber-preprocessor";
-
+import {
+  Given,
+  Step,
+  Then,
+  When,
+} from "@badeball/cypress-cucumber-preprocessor";
 
 // Background: Accessing camera app for facial recognition
 
-Given("the user is at the transaction history page with the deposit and withdraw buttons", () => {
-    cy.intercept('GET', Cypress.env('transactions_uri')).as('getTransactions');
-    cy.visit('/');
-    // cy.wait('@getTransactions');
-})
+Given(
+  "the user is at the transaction history page with the deposit and withdraw buttons",
+  () => {
+    cy.login("ABC123", "1234");
+    cy.visit("/");
+    cy.wait(1000);
+  }
+);
 
 When("he clicks on the deposit or withdrawal button", () => {
-    // cy.contains('WITHDRAW').click();
-})
+  cy.contains("WITHDRAW").click();
+});
 
-Then("he is directed to authenticate", () => {
-    // cy.url().should('include', '/camera');
-})
-
-Then('he sees a loading screen while the software accesses the camera', () => {
-    // cy.contains('Loading');
-})
+Then("he sees a loading screen while the software accesses the camera", () => {
+  cy.contains("Loading...");
+});
 
 Then("he is directed to the facial authentication page", () => {
-    // cy.get('video', {timeout: 10000});
-    // cy.wait(1000);
-})
-
+  cy.get("video", { timeout: 10000 });
+  cy.wait(1000);
+});
 
 // Scenario: Using facial recognition
+
 Given("the user is the correct owner of the account", () => {
-    cy.intercept('POST', Cypress.env('face_auth_uri'), {
-        body: {
-            "authenticated": true
-        }
-    });
-})
+  cy.intercept("POST", `${Cypress.env("face_auth_uri")}`, {
+    body: {
+      authenticated: true,
+    },
+  });
+});
 
 When("he looks at the camera and presses the authentication button", () => {
-    // cy.contains('AUTH NOW').click();
-})
+  cy.contains("AUTH NOW").click();
+});
 
 Then("the user will be redirected to the success page", () => {
-    cy.contains('try again').should('not.exist');
-})
-
+  cy.contains("try again").should("not.exist");
+});
 
 // Scenario: Using facial recognition but wrong user
 
 Given("the user is not the account owner", () => {
-    cy.intercept('POST', Cypress.env('face_auth_uri'), {
-        body: {
-            "authenticated": false
-        }
-    });
-})
+  cy.intercept("POST", Cypress.env("face_auth_uri"), {
+    body: {
+      authenticated: false,
+    },
+  });
+});
 
-Then('the user will receive an "Authentication Failed, incorrect user" message', () => {
-    // cy.contains('Authentication failed, incorrect user').should('be.visible');
-})
-
+Then(
+  'the user will receive an "Authentication Failed, incorrect user" message',
+  () => {
+    cy.contains("Authentication failed, incorrect user").should("be.visible");
+  }
+);
 
 //Scenario: User cannot authenticate via face
-When("face authentication fails thrice", () => {
-    Step(this, 'the user is not the account owner');
-    Step(this, 'he looks at the camera and presses the authentication button');
-    cy.wait(500);
-    Step(this, 'he looks at the camera and presses the authentication button');
-    cy.wait(500);
-    Step(this, 'he looks at the camera and presses the authentication button');
-    cy.wait(500);
-})
 
-Then('he will be redirected to the pin authentication page', () => {
-    cy.wait(1000);
-    // cy.get('input', {timeout: 25000}).should('have.length', 4);
-    // cy.get('button');
-})
+When("face authentication fails thrice", () => {
+  Step(this, "the user is not the account owner");
+  Step(this, "he looks at the camera and presses the authentication button");
+  cy.wait(500);
+  Step(this, "he looks at the camera and presses the authentication button");
+  cy.wait(500);
+  Step(this, "he looks at the camera and presses the authentication button");
+  cy.wait(500);
+});
+
+Then("he will be redirected to the pin authentication page", () => {
+  cy.wait(1000);
+  cy.get("input", { timeout: 25000 }).should("have.length", 4);
+  cy.get("button");
+});
 
 // Scenario: User authenticates succesfully with his pin
-Given('the user is at the pin authentication page', () => {
-    Step(this, 'face authentication fails thrice');
-    Step(this, 'he will be redirected to the pin authentication page');
-})
+
+Given("the user is at the pin authentication page", () => {
+  Step(this, "face authentication fails thrice");
+  Step(this, "he will be redirected to the pin authentication page");
+});
 
 When("he enters the correct pin number", () => {
-    const pin = '1234';
-    for (let i = 0; i < pin.length; i++) {
-        const char = pin.charAt(i);
-        // cy.get('input').eq(i).type(char);
-        cy.wait(100);
-    }
-    cy.intercept('POST', Cypress.env('pin_auth_uri'), {
-        body: {
-            "authenticated": true,
-        }
-    });
-    // cy.contains('AUTH NOW').click();
-})
+  const pin = "1234";
+  for (let i = 0; i < pin.length; i++) {
+    const char = pin.charAt(i);
+    cy.get("input").eq(i).type(char);
+    cy.wait(100);
+  }
+  cy.intercept("POST", Cypress.env("pin_auth_uri"), {
+    body: {
+      authenticated: true,
+    },
+  });
+  cy.contains("AUTH NOW").click();
+});
 
 Then("he will be directed to a success page", () => {
-    // cy.contains('Success');
-})
+  cy.contains("RETURN TO HOMEPAGE");
+});
 
-//Scenario: User cannot authenticate via face and wants forgets his login pin
+// Scenario: User cannot authenticate via face and wants forgets his login pin
+
 When("he enters the wrong pin number", () => {
-    const pin = '1111';
-    for (let i = 0; i < pin.length; i++) {
-        const char = pin.charAt(i);
-        // cy.get('input').eq(i).type(char);
-        cy.wait(100);
-    }
-    cy.intercept('POST', Cypress.env('pin_auth_uri'), {
-        body: {
-            "authenticated": false,
-        }
-    });
-    // cy.contains('AUTH NOW').click();
-})
+  const pin = "1111";
+  for (let i = 0; i < pin.length; i++) {
+    const char = pin.charAt(i);
+    cy.get("input").eq(i).type(char);
+    cy.wait(100);
+  }
+  cy.intercept("POST", Cypress.env("pin_auth_uri"), {
+    body: {
+      authenticated: false,
+    },
+  });
+  cy.contains("AUTH NOW").click();
+});
 
 Then("he will see a message that indicates his pin is wrong", () => {
-    // cy.contains('Incorrect Pin');
-})
+  cy.contains("Incorrect Pin");
+});
 
 Then("he will not be directed to a success page", () => {
-    // cy.contains('Success').should('not.exist');
-})
+  cy.contains("Success").should("not.exist");
+});
--- frontend/cypress/e2e/transactionhistory/transactionhistory.js
+++ frontend/cypress/e2e/transactionhistory/transactionhistory.js
@@ -1,76 +1,79 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
-Given("I am a registered user with past transactions", () => {
-  cy.login("S1234567G", "0000");
+// Background: Registered user
+
+Given("I am a registered user", () => {
+  cy.login("ABC123", "1234");
 });
 
-Given("I visit the homepage", () => {
-  cy.intercept("GET", `${Cypress.env("backend_uri")}/transactions`, {
+// Scenario: Launching the web app and viewing familiar transaction records
+
+Given("I have past transactions", () => {
+  cy.intercept("GET", "api/user/getTable", {
     statusCode: 200,
     fixture: "transactions.json",
   });
-  cy.visit("/");
+});
+
+Given("I visit the homepage", () => {
+  cy.visit("/transactionHistory");
 });
 
 Then(
   "I should see the homepage with my most recent transaction at the top",
   () => {
-    // cy.get("#transactions tbody tr:first")
-    //   .find("td")
-    //   // .then(($td) => {
-    //   //     return {
-    //   //         date: Date($td[0].innerText),
-    //   //         amount: Number($td[1].innerText),
-    //   //         bal: Number($td[2].innerText)
-    //   //     }
-    //   // })
-    //   // .should('deep.equal', {
-    //   //     date: Date('2023-07-05'),
-    //   //     amount: 1,
-    //   //     bal: 1440
-    //   // })
-    //   .then(($td) => {
-    //     return {
-    //       date: $td[0].innerText,
-    //       amount: $td[1].innerText,
-    //       bal: $td[2].innerText,
-    //     };
-    //   })
-    //   .should("deep.equal", {
-    //     date: "7/5/2023",
-    //     amount: "+1.0",
-    //     bal: "1440.00",
-    //   });
+    cy.get("#transactions tbody tr:first")
+      .find("td")
+      .then(($td) => {
+        return {
+          date: $td[0].innerText,
+          amount: $td[1].innerText,
+          bal: $td[2].innerText,
+        };
+      })
+      .should("deep.equal", {
+        date: "7/5/2023",
+        amount: "+1.0",
+        bal: "1440.00",
+      });
   }
 );
 
+// Scenario: Reload the web page to retry getting transaction records
+
 Given("I am unable to connect to the server", () => {
-  cy.intercept("GET", `${Cypress.env("backend_uri")}/transactions`, {
+  cy.intercept("GET", "api/user/getTable", {
     statusCode: 400,
   });
 });
 
 Then("I will see an empty screen", () => {
-  cy.get("table").should("not.exist");
+  cy.get("#transactions tbody tr").should("have.length", 0);
+  // cy.get("table").should("not.exist");
 });
 
 Given("an error message", () => {
-  // cy.contains("Sorry our services seem to be down at the moment");
+  cy.contains("Sorry our services seem to be down at the moment");
 });
 
 Then("a suggestion to rectify the error", () => {
-  // cy.contains("Try again in around 30 mins");
+  cy.contains("Try again in around 30 mins");
 });
 
 Then("a button to refresh", () => {
-  // cy.get("#refreshButton").click();
-  // cy.url().should("be.equal", `${Cypress.config("baseUrl")}/`);
+  cy.get("#refreshButton").click();
+  cy.url().should(
+    "be.equal",
+    `${Cypress.config("baseUrl")}/transactionHistory`
+  );
 });
 
+// Scenario: Viewing earlier transactions
+
 When("I scroll up the transaction panel", () => {
-  // cy.get("#transactions").parent().scrollTo("bottom");
+  cy.get("#transactions").parent().scrollTo("bottom");
 });
 
 Then("I should see my earlier transactions", () => {
-  // cy.get("#transactions tbody tr").last().should("be.visible");
+  cy.get("#transactions tbody tr").last().should("be.visible");
 });
