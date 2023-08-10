a982f56fa31dd54afc15b95d6c48f8b3fcebf0e5
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-08-11 02:22:11 +0800

    update logout api pages

--- frontend/pages/api/cookie/logout.js
+++ frontend/pages/api/cookie/logout.js
@@ -5,17 +5,24 @@ export default async function handler(req, res) {
     return res.status(405).end();
   }
   console.log("calling server");
+  const userCookie = req.headers.cookie;
+
   try {
     const response = await fetch(
       `https://backend-dbs-grp7-ml42q3c3ya-as.a.run.app/logout`,
       {
         method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${userCookie}`, // Example: using cookie value as a Bearer token
+          Cookie: userCookie, // Or you could forward the cookie directly, if the external API expects it
+        },
         credentials: "include", // This will ensure cookies are sent with the request
       }
     );
 
     if (!response.ok) {
       const data = await response.json();
+      console.log("COOKIE NOT CLEARED");
       throw new Error(data.message || "Failed to log out");
     }
 
@@ -25,6 +32,7 @@ export default async function handler(req, res) {
       "Set-Cookie",
       "_your_app_session=; Max-Age=0; Path=/; HttpOnly"
     );
+    console.log("COOKIE CLEARED");
 
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
